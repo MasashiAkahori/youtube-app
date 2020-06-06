5d95db9f0f7f36a2b805f6ab2e9f32a065b8fb5b
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2020-06-07 03:11:31 +0900

    Initial Commit

---
+++ youtube-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-app
+//
+//  Created by 赤堀雅司 on 7/6/20.
+//  Copyright © 2020 赤堀雅司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
